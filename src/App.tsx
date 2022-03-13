0e6a3faec619e4fe474381d66bda57ef761765f4
Fernando Tiburcio <fernando.tiburcio@gmail.com>
2022-03-13 17:18:52 -0300

    chore: add styled-components and theme to app add Roboto font add expo-app-loading

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components/native";
+
+import Home from './screens/Home';
+
+import THEME from "./theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={THEME}>
+    <SafeAreaView style={{flex: 1}}>
+      <Home />
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+    </SafeAreaView>
+
+    </ThemeProvider>
+  );
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#1D5A93',
+    DARK_TEXT: '#34393E',
+    LIGHT_TEXT: '#FFFFFF',
+    ALERT: '#E11D48',
+    WARNING: '#FCD34D'
+  },
+  FONTS: {
+    REGULAR: 'Roboto_400Regular',
+    MEDIUM: 'Roboto_500Medium',
+    BOLD: 'Roboto_700Bold',
+  },
+  FONT_SIZES: {
+    XS: '10px',
+    SM: '14px',
+    RG: '16px',
+    MD: '20px',
+    BG: '26px',
+    XL: '32px',
+  },
+};
