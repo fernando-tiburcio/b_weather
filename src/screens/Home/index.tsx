5e451c2ab588a411e3f72c33277db7a01039bfd6
Fernando Tiburcio <fernando.tiburcio@gmail.com>
2022-03-13 17:19:24 -0300

    feat: create Home component as a root screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Header } from '../../components/Header';
+import { Container } from './styles';
+
+export default function Home(){
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
