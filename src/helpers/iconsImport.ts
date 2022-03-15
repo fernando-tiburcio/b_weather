1bb9cf75e95c1d3aeef53245ede27d51d4377bdb
Fernando Tiburcio <fernando.tiburcio@agriness.com>
2022-03-15 07:38:58 -0300

    feat: add icons and create icons lib

---
+++ src/helpers/iconsImport.ts
@@ -0,0 +1,33 @@
+import O1D from '../assets/icons/01d.png'
+import O2D from '../assets/icons/02d.png'
+import O3D from '../assets/icons/03d.png'
+import O9D from '../assets/icons/09d.png'
+import O11D from '../assets/icons/11d.png'
+import O13D from '../assets/icons/13d.png'
+import O50D from '../assets/icons/50d.png'
+import O1N from '../assets/icons/01n.png'
+import O2N from '../assets/icons/02n.png'
+import O3N from '../assets/icons/03n.png'
+import O9N from '../assets/icons/09n.png'
+import O11N from '../assets/icons/11n.png'
+import O13N from '../assets/icons/13n.png'
+import O50N from '../assets/icons/50n.png'
+
+export default {
+  '01d': O1D,
+  '02d': O2D,
+  '03d': O3D,
+  '04d': O3D,
+  '09d': O9D,
+  '11d': O11D,
+  '13d': O13D,
+  '50d': O50D,
+  '01n': O1N,
+  '02n': O2N,
+  '03n': O3N,
+  '04n': O3N,
+  '09n': O9N,
+  '11n': O11N,
+  '13n': O13N,
+  '50n': O50N,
+};
